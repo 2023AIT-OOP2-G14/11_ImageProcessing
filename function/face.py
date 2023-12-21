35ad83268d6918c627b3b9c7cea59cc3994d2454
niwa100 <153746376+niwa100@users.noreply.github.com>
2023-12-21 16:15:00 +0900

    仮追加

--- function/face.py
+++ function/face.py
@@ -3,3 +3,11 @@ import cv2 as cv
 class frame:
     def __init__(self) -> None:
         pass
+
+    def face_detection(self, file_name):
+        pass
+        
+        
+if __name__ == '__main__':
+    fr = frame()
+    frame.face_detection()
