7e992997466406700dad70eb8b8fafc813906de8
nekonekocatcat <n0k2s0m1+github@gmail.com>
2023-12-21 16:15:10 +0900

    モザイククラス追加

--- function/mosaic.py
+++ function/mosaic.py
@@ -1,5 +1,17 @@
-import cv2 as cv
+import cv2
 
 class mosaic:
     def __init__(self) -> None:
         pass
+
+    def mosaic_face(self,file_name):
+        image = cv2.IMREAD_GRAYSCALE(file_name)
+        
+
+if __name__ == '__main__':
+    Mosaic = mosaic()
+
+
+
+
+
