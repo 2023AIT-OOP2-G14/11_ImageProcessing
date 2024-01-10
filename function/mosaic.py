0e4c342223c96aa4abc03a4b2051209bd3fcdb9e
nekonekocatcat <n0k2s0m1+github@gmail.com>
2024-01-10 16:46:46 +0900

    ****を書いた

--- function/mosaic.py
+++ function/mosaic.py
@@ -4,7 +4,7 @@ import numpy as np
 from yunet import YuNet
 
 class mosaic:
-    def __init__(self, ****************):
+    def __init__(self, model_path='./face_detection_yunet_2023mar.onnx', input_size=[320, 320], conf_threshold=0.9, nms_threshold=0.3, top_k=5000):
         """
         FaceDetectorクラスのコンストラクタです.
 
@@ -23,13 +23,13 @@ class mosaic:
         """
         # YuNetモデルのインスタンスを作成します.
         self.model = YuNet(
-            modelPath=*********,        # modelPath: モデルのパス
-            inputSize=*********,        # inputSize: モデルの入力サイズ
-            confThreshold=*********,    # 信頼度の閾値。この値以上の信頼度を持つバウンディングボックスのみが結果として出力されます。
-            nmsThreshold=*********,     # 非最大抑制(NMS)の閾値。この値以上のIoUを持つバウンディングボックスは、NMSにより抑制されます。
-            topK=*********,             # 上位のバウンディングボックスの数. この数だけバウンディングボックスが出力されます。
-            backendId=********,         # バックエンドのID. ここではOpenCVのDNNバックエンドを使用します。(例: cv.dnn.DNN_BACKEND_OPENCV)
-            targetId=*********          # ターゲットのID ここではCPUをターゲットとします。(例: cv.dnn.DNN_TARGET_CPU)
+            modelPath=model_path,        # modelPath: モデルのパス
+            inputSize=input_size,        # inputSize: モデルの入力サイズ
+            confThreshold=conf_threshold,    # 信頼度の閾値。この値以上の信頼度を持つバウンディングボックスのみが結果として出力されます。
+            nmsThreshold=nms_threshold,     # 非最大抑制(NMS)の閾値。この値以上のIoUを持つバウンディングボックスは、NMSにより抑制されます。
+            topK=top_k,             # 上位のバウンディングボックスの数. この数だけバウンディングボックスが出力されます。
+            backendId=cv.dnn.DNN_BACKEND_OPENCV,         # バックエンドのID. ここではOpenCVのDNNバックエンドを使用します。(例: cv.dnn.DNN_BACKEND_OPENCV)
+            targetId=cv.dnn.DNN_TARGET_CPU          # ターゲットのID ここではCPUをターゲットとします。(例: cv.dnn.DNN_TARGET_CPU)
         )
     
     def face_detection(self, file_name):
@@ -56,16 +56,16 @@ class mosaic:
         信頼度が指定された閾値（conf_threshold）よりも低い場合、その顔の検出結果は無視されます.
         """
         # 画像を読み込みます. ここではOpenCVのimread関数を使用しています. 
-        ************
+        image = cv.imread(file_name)
         # 画像の形状（高さ、幅、チャンネル数）を取得します.
-        h, w, _ = ***********
+        h, w, _ = image.shape
         # モデルの入力サイズを画像の幅と高さに設定します.
-        **************
+        self.model.setInputSize((w , h))
         # モデルを使って画像から顔を検出します.
-        results = *************
+        results = self.model.infer(image)
         return results
 
-    def mosaic_face(self,file_name, ********):
+    def mosaic_face(self,file_name, factor = 20):
         """
         与えられた画像に対して顔を検出し、顔部分をモザイク処理します.
 
@@ -77,30 +77,30 @@ class mosaic:
             モザイク処理の程度を示す係数 (デフォルト: 20)
         """
         # 画像を読み込みます. face_detectionを使って顔を検出します.
-        detected_faces = ************
+        detected_faces = self.face_detection(file_name)
         # 画像を読み込みます. ここではOpenCVのimread関数を使用しています.
-        image = ***********
+        image = cv.imread(file_name)
         # 元の画像のコピーを作成します. このコピーに対して変更を加えます.
-        output = *********
+        output = image.copy()
 
         # 検出結果を反復処理します. 各検出結果は、バウンディングボックスの座標を含む配列です.
-        for *********
+        for det in detected_faces :
             bbox = det[0:4].astype(np.int32)    # バウンディングボックスの座標を整数に変換します.
             # バウンディングボックスの座標を使って顔部分を切り出します.
-            x, y, w, h = *********
+            x, y, w, h = bbox
 
             # 顔部分をモザイク処理します.
-            face_roi = ***************  # 顔部分を切り出します.
+            face_roi = image[y : y + h , x : x + w]  # 顔部分を切り出します.
             small = cv.resize(face_roi, None, fx=1.0 / factor, fy=1.0 / factor, interpolation=cv.INTER_NEAREST) # 顔部分を縮小します.
             mosaic_face = cv.resize(small, (w, h), interpolation=cv.INTER_NEAREST)  # 顔部分を元のサイズに戻します.
             output[y:y + h, x:x + w] = mosaic_face  # モザイク処理した顔部分を元の画像に貼り付けます.
         # 矩形で囲まれた画像を保存する
-        ************
+        cv.imwrite('mosaic_output.jpg' , output)
 
 
 if __name__ == '__main__':
     Mosaic = mosaic()
-    Mosaic.mosaic_face(*********)
+    Mosaic.mosaic_face('input_image.jpg')
 
 
 
